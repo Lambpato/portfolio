4c34cccb873bc90a3db6bcc992bed0edc026bc48
Lamberto Martinez <lambpato@gmail.com>
2023-10-10 19:40:29 +0000

    added clickable links to the skill icons

--- portfolio/src/components/SkillsComp.vue
+++ portfolio/src/components/SkillsComp.vue
@@ -3,99 +3,122 @@ const icons = [
   {
     id: 1,
     name: 'React',
-    url: '/src/components/icons/react.png'
+    imgUrl: '/src/components/icons/react.png',
+    siteUrl: 'https://react.dev/'
   },
   {
     id: 2,
     name: 'Next.js',
-    url: '/src/components/icons/next-js.png'
+    imgUrl: '/src/components/icons/next-js.png',
+    siteUrl: 'https://nextjs.org/'
   },
   {
     id: 3,
     name: 'Vue.js',
-    url: '/src/components/icons/vue.png'
+    imgUrl: '/src/components/icons/vue.png',
+    siteUrl: 'https://vuejs.org/'
   },
   {
     id: 4,
     name: 'TypeScript',
-    url: '/src/components/icons/typescript.png'
+    imgUrl: '/src/components/icons/typescript.png',
+    siteUrl: 'https://www.typescriptlang.org/'
   },
   {
     id: 5,
     name: 'JavaScript',
-    url: '/src/components/icons/javascript.png'
+    imgUrl: '/src/components/icons/javascript.png',
+    siteUrl: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
   },
   {
     id: 6,
     name: 'HTML',
-    url: '/src/components/icons/html.png'
+    imgUrl: '/src/components/icons/html.png',
+    siteUrl: 'https://developer.mozilla.org/en-US/docs/Web/HTML'
   },
   {
     id: 7,
     name: 'CSS',
-    url: '/src/components/icons/css.png'
+    imgUrl: '/src/components/icons/css.png',
+    siteUrl: 'https://developer.mozilla.org/en-US/docs/Web/CSS'
   },
   {
     id: 8,
     name: 'Bootstrap',
-    url: '/src/components/icons/bootstrap.png'
+    imgUrl: '/src/components/icons/bootstrap.png',
+    siteUrl: 'https://getbootstrap.com/'
   },
   {
     id: 9,
     name: 'Tailwind',
-    url: '/src/components/icons/tailwind.png'
+    imgUrl: '/src/components/icons/tailwind.png',
+    siteUrl: 'https://tailwindcss.com/'
   },
   {
     id: 10,
     name: 'Express',
-    url: '/src/components/icons/express.png'
+    imgUrl: '/src/components/icons/express.png',
+    siteUrl: 'https://expressjs.com/'
   },
   {
     id: 11,
     name: 'Node.js',
-    url: '/src/components/icons/node-js.png'
+    imgUrl: '/src/components/icons/node-js.png',
+    siteUrl: 'https://nodejs.org/en'
   },
   {
     id: 12,
     name: 'PostgreSQL',
-    url: '/src/components/icons/postgresql.png'
+    imgUrl: '/src/components/icons/postgresql.png',
+    siteUrl: 'https://www.postgresql.org/'
   },
   {
     id: 13,
     name: 'Prisma',
-    url: '/src/components/icons/prisma.png'
+    imgUrl: '/src/components/icons/prisma.png',
+    siteUrl: 'https://www.prisma.io/'
   },
   {
     id: 14,
     name: 'Azure',
-    url: '/src/components/icons/azure.png'
+    imgUrl: '/src/components/icons/azure.png',
+    siteUrl: 'https://azure.microsoft.com/en-us'
   },
   {
     id: 15,
     name: 'NPM',
-    url: '/src/components/icons/npm.png'
+    imgUrl: '/src/components/icons/npm.png',
+    siteUrl: 'https://www.npmjs.com/'
   },
   {
     id: 16,
     name: 'Webpack',
-    url: '/src/components/icons/webpack.png'
+    imgUrl: '/src/components/icons/webpack.png',
+    siteUrl: 'https://webpack.js.org/'
   },
   {
     id: 17,
     name: 'Git',
-    url: '/src/components/icons/git.png'
+    imgUrl: '/src/components/icons/git.png',
+    siteUrl: 'https://git-scm.com/'
   },
   {
     id: 18,
     name: 'Arduino',
-    url: '/src/components/icons/arduino.png'
+    imgUrl: '/src/components/icons/arduino.png',
+    siteUrl: 'https://www.arduino.cc/'
   },
   {
     id: 19,
     name: 'Figma',
-    url: '/src/components/icons/figma.png'
+    imgUrl: '/src/components/icons/figma.png',
+    siteUrl: 'https://www.figma.com/'
   }
 ]
+
+const redirectToWebsite = (url: string) => {
+  window.open(url, '_blank')
+}
 </script>
 
 <template>
@@ -104,11 +127,16 @@ const icons = [
       <h1 class="text-center text-7xl">Skills</h1>
     </header>
     <div id="icons" class="grid grid-cols-3 lg:grid-cols-4 gap-y-4">
-      <div v-for="icon in icons" :key="icon.id" class="flex justify-center items-center">
+      <div
+        v-for="icon in icons"
+        :key="icon.id"
+        @click="redirectToWebsite(icon.siteUrl)"
+        class="flex justify-center items-center"
+      >
         <div
           class="flex flex-col justify-center items-center w-2/4 h-[160px] border-4 rounded-xl border-slate-200"
         >
-          <img :src="icon.url" :alt="icon.name" class="w-24 p-4" />
+          <img :src="icon.imgUrl" :alt="icon.name" class="w-24 p-4" />
           <h1 class="font-bold">{{ icon.name }}</h1>
         </div>
       </div>
