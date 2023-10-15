62117e92db12932cb45a944dd9b8c4c76cf77100
Lamberto Martinez <lambpato@gmail.com>
2023-10-15 19:42:42 +0000

    compeleted portfolio

--- portfolio/src/components/HomeComp.vue
+++ portfolio/src/components/HomeComp.vue
@@ -3,16 +3,11 @@ import IntroComp from './IntroComp.vue'
 import SkillsComp from './SkillsComp.vue'
 import ProjectsComp from './ProjectsComp.vue'
 import QualificationsComp from './QualificationsComp.vue'
-import router from '@/router'
-
-const handleAbout = () => {
-  router.push({ path: '/about-me' })
-}
 </script>
 
 <template>
   <div class="flex flex-col gap-20">
-    <IntroComp -section="Introduction" class="cursor-pointer" @click="handleAbout" />
+    <IntroComp -section="Introduction" class="cursor-pointer" />
     <SkillsComp />
     <ProjectsComp />
     <QualificationsComp />
