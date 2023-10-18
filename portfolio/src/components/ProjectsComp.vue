ab146fc003750ae1687923d5ed0419bf5afe781d
Lamberto Martinez <lambpato@gmail.com>
2023-10-18 07:12:01 +0000

    fixed project layout on mobile in home screen

--- portfolio/src/components/ProjectsComp.vue
+++ portfolio/src/components/ProjectsComp.vue
@@ -34,12 +34,12 @@ const projects = [
         class="flex justify-center items-center"
       >
         <div
-          class="flex flex-col items-center mt-0 w-2/4 lg:h-[300px] border-4 rounded-xl border-slate-200 cursor-pointer"
+          class="flex flex-col items-center mt-0 w-11/12 lg:w-2/4 lg:h-[300px] border-4 rounded-xl border-slate-200 cursor-pointer"
         >
           <img :src="project.thumbnail" :alt="project.name" class="w-auto rounded-lg" />
           <div class="m-auto text-center">
-            <h1 class="font-bold text-xs lg:text-lg">{{ project.name }}</h1>
-            <p>Web Application</p>
+            <h1 class="font-bold lg:text-xs lg:text-lg">{{ project.name }}</h1>
+            <p class="sm:text-xxs">Web Application</p>
           </div>
         </div>
       </div>
