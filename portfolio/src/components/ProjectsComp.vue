37fb6688bff951a2b6460488cf2b1adf60229440
Lamberto Martinez <lambpato@gmail.com>
2023-10-20 21:02:33 +0000

    changed function to be more sematic and added the @click into the actual project

--- portfolio/src/components/ProjectsComp.vue
+++ portfolio/src/components/ProjectsComp.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import router from '@/router'
 
-const handleClick = (href: string) => {
+const goToView = (href: string) => {
   router.push({ path: href })
 }
 
@@ -27,14 +27,10 @@ const projects = [
       <h1 class="text-center text-5xl lg:text-7xl">Projects</h1>
     </header>
     <div id="icons" class="grid grid-cols-2 lg:grid-cols-2 gap-y-4">
-      <div
-        v-for="project in projects"
-        :key="project.id"
-        @click="() => handleClick(project.href)"
-        class="flex justify-center items-center"
-      >
+      <div v-for="project in projects" :key="project.id" class="flex justify-center items-center">
         <div
           class="flex flex-col items-center mt-0 w-11/12 lg:w-2/4 lg:h-[300px] border-4 rounded-xl border-slate-200 hover:bg-slate-600 cursor-pointer"
+          @click="() => goToView(project.href)"
         >
           <img :src="project.thumbnail" :alt="project.name" class="w-auto rounded-lg" />
           <div class="m-auto text-center">
